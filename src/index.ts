7af68e949ceba0089e1c5d656c8cf2cebe8db87b
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-10-21 14:41:44 -0400

    Make it work

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,27 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
-
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, /*env: Env,*/ ctx: ExecutionContext): Promise<Response> {
+		let url = (new URL(request.url)).searchParams.get('url');
+		if (url == null) {
+			console.log("no url param")
+			return new Response(null);
+		} else {
+			console.log("proxying" + url)
+			var request: Request = new Request(url)
+			// set origin header just in case
+			request.headers.append('Origin', new URL(url).origin)
+			const response: Response = await fetch(request);
+
+			// essentially disable cors
+			const responseHeaders: Headers = new Headers();
+			responseHeaders.append('Access-Control-Allow-Origin', '*');
+			responseHeaders.append('Access-Control-Allow-Methods', '*');
+			responseHeaders.append('Access-Control-Allow-Headers', '*');
+			responseHeaders.append('Access-Control-Allow-Credentials', 'true');
+
+			var modifiedResponse: Response = new Response(response.body, {
+				headers: responseHeaders,
+			});
+			return modifiedResponse;
+		}
 	},
 };
